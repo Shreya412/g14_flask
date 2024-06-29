c08d5cfbfb45531f2b4b8ca0cd1682a57f2ce960
Shreya412 <4shreyap@gmail.com>
2024-06-28 18:37:34 -0700

    remove venv

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,7 @@ sudo mv env .env
 sudo apt-get update
 echo "installing python and pip"
 sudo apt-get install -y python3 python3-pip
-python3 -m venv venv
-# Activate the virtual environment
-source venv/bin/activate
+
 # Install application dependencies from requirements.txt
 echo "Install application dependencies from requirements.txt"
 pip install -r requirements.txt
