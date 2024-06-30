dda3329d1843c1b5f366fc8c787096a5b8f69db7
Shreya412 <4shreyap@gmail.com>
2024-06-29 17:31:59 -0700

    stream csv data

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file, send_from_director
 import os
 from utils.utils import app_gen, exp1, exp2, exp3, exp4
 import pandas as pd
+import io
 
 app = Flask(__name__, template_folder='templates')
 UPLOAD_FOLDER = 'uploads'
@@ -13,40 +14,43 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    requests = request.form['options']
-    print(requests, "request")
+    option = request.form['options']
     if 'file' not in request.files:
         return "No file part"
     file = request.files['file']
     if file.filename == '':
         return "No selected file"
     if file:
-        filename = file.filename
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        df = pd.read_csv(file_path, delimiter=',', header=None)
+        # Read the uploaded file as a binary stream
+        file_stream = io.BytesIO(file.read())
+        df = pd.read_csv(file_stream, delimiter=',', header=None)
         results = []
         for column in df.columns:
             col_values = df[column].values
             if len(col_values) >= 5:
-                if requests == 'exp1':
+                if option == 'exp1':
                     result = exp1(col_values[:5])
-                elif requests == 'exp2':
+                elif option == 'exp2':
                     result = exp2(col_values[:5])
-                elif requests == 'exp3':
+                elif option == 'exp3':
                     result = exp3(col_values[:5])
-                elif requests == 'exp4':
+                elif option == 'exp4':
                     result = exp4(col_values[:5])
-                elif requests == 'app_gen':
+                elif option == 'app_gen':
                     result = app_gen(col_values[:5])
                 else:
                     return "Invalid request"
                 results.append(result)
         results_df = pd.DataFrame([results])
-        results_csv_path = os.path.join(app.config['UPLOAD_FOLDER'], f'{requests}.csv')
-        results_df.to_csv(results_csv_path, index=False, header=False)
-        print(f"Results saved to '{requests}.csv'.")
-        return send_file(results_csv_path, as_attachment=True)
+        results_csv_stream = io.StringIO()
+        results_df.to_csv(results_csv_stream, index=False, header=False)
+        results_csv_stream.seek(0)
+        return send_file(
+            io.BytesIO(results_csv_stream.getvalue().encode()),
+            mimetype='text/csv',
+            as_attachment=True,
+            download_name=f'{option}.csv'
+        )
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
